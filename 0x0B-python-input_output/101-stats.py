3349ac5758784c5b2218dbe5ad0f3c1193c556ad
iamgideonchrist <attakpahgideon@gmail.com>
2023-06-13 13:26:40 -0700

    Log parsing

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+# 101-stats.py
+# iamgideonchrist <142@alx.com>
 """Reads from standard input and computes metrics.
 
 After every ten lines or the input of a keyboard interruption (CTRL + C),
